77cf9109d3fd6339d7fd7a4e6ae904aadfa2bcc0
GabrielHenriqueFurlan <gabriel.henrique.furlan.85@gmail.com>
2024-07-03 16:53:31 -0300

    Add files via upload

---
+++ Operadores.java
@@ -0,0 +1,39 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        // Operador de atribuição: =
+        System.out.println("Operador de atribuição: =");
+        String texto = "Texto";
+        System.out.println("String texto = \"Texto\";");
+        System.out.println(texto);  // Imprime: Texto
+
+        // Operador de soma: +
+        System.out.println("\nOperador de soma: +");
+        int soma = 1 + 1;
+        System.out.println("int soma = 1 + 1;");
+        System.out.println(soma);   // Imprime: 2
+
+        // Operador de subtração: -
+        System.out.println("\nOperador de subtração: -");
+        int subtracao = 1 - 1;
+        System.out.println("int subtracao = 1 - 1;");
+        System.out.println(subtracao);  // Imprime: 0
+
+        // Operador de divisão: /
+        System.out.println("\nOperador de divisão: /");
+        int divisao = 1 / 1;
+        System.out.println("int divisao = 1 / 1;");
+        System.out.println(divisao);    // Resultado: 1
+
+        // Demonstração do operador de multiplicação: *
+        System.out.println("\nOperador de multiplicação: *");
+        int multiplicacao = 1 * 1;
+        System.out.println("int multiplicacao = 1 * 1;");
+        System.out.println(multiplicacao);  // Resultado: 1
+
+        // Operador de concatenação: +
+        System.out.println("\nOperador de concatenação de strings: +");
+        String concatenacao = "Texto 1 + " + "Texto 2";
+        System.out.println("String concatenacao = \"Texto 1 + \" + \"Texto 2\";");
+        System.out.println(concatenacao);  // Resultado: Texto 1 + Texto 2
+    }
+}
